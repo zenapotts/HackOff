b25afed5d11c581cb34c613f69806013c3b94ec2
Zena Potts <zpotts@freshbooks.com>
2017-07-06 16:25:55 -0400

    function

--- display.php
+++ display.php
@@ -13,9 +13,23 @@
 //   		}
 //   	});
 // );
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 $(document).ready(function () {
+	var country = getParameterByName('country'),
+	city = getParameterByName('city'),
+	profession = getParameterByName('profession');
+	
 	$.get(
-		$(this).data('http://docker:4568/service/business_lookup/<city>/<country>/<profession>'),
+		$(this).data('http://docker:4568/service/business_lookup/' + city + '/' + country + '/' + profession),
 		function (data) {
 			console.log(data);
 		}
--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <html>
 <link rel="stylesheet" type="text/css" href="home.css" media="screen" />
 <body>
-<pre><input id="small-business-search-field" type="text" class="ui-input" src="/_themes/freshbooks/smux-assets/img/icons/search.svg" placeholder="Enter Country"> <input id="small-business-search-field" type="text" class="ui-input" placeholder="Enter City"> <input id="small-business-search-field" type="text" class="ui-input" placeholder="Enter Industry"> <a href="display.php" style="text-decoration:none"><button class="button">Search</button></a>
+<form action="display.php" method="post">
+<pre>
+	<input id="small-business-search-field" type="text" class="ui-input" src="/_themes/freshbooks/smux-assets/img/icons/search.svg" placeholder="Enter Country" name="country"> 
+	<input id="small-business-search-field" type="text" class="ui-input" placeholder="Enter City" name="city"> 
+	<input id="small-business-search-field" type="text" class="ui-input" placeholder="Enter Industry" name="profession">
+	<!-- <a href="display.php" style="text-decoration:none"><button class="button">Search</button></a> -->
+	<button type="submit">Search</button>
 </pre>
+</form>
 </body>
 </html>
