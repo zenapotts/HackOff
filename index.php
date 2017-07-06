f27bbe95b9d8990830d3bc96efabb0ea85009b05
Zena Potts <zpotts@freshbooks.com>
2017-07-06 16:31:16 -0400

    a lot

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <html>
 <link rel="stylesheet" type="text/css" href="home.css" media="screen" />
 <body>
-<form action="display.php" method="post">
-<pre>
-	<input id="small-business-search-field" type="text" class="ui-input" src="/_themes/freshbooks/smux-assets/img/icons/search.svg" placeholder="Enter Country" name="country"> 
-	<input id="small-business-search-field" type="text" class="ui-input" placeholder="Enter City" name="city"> 
-	<input id="small-business-search-field" type="text" class="ui-input" placeholder="Enter Industry" name="profession">
-	<!-- <a href="display.php" style="text-decoration:none"><button class="button">Search</button></a> -->
-	<button type="submit">Search</button>
+<pre><input id="small-business-search-field" type="text" class="ui-input country" src="/_themes/freshbooks/smux-assets/img/icons/search.svg" placeholder="Enter Country"> <input id="small-business-search-field" type="text" class="ui-input city" placeholder="Enter City"> <input id="small-business-search-field" type="text" class="ui-input profession" placeholder="Enter Industry"> <a href="#" class="submit" style="text-decoration:none"><button class="button">Search</button></a>
 </pre>
-</form>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+	$('.submit').click(function () {
+		var country = $('.country').val(),
+			city = $('.city').val(),
+			profession = $('.profession').val();
+
+		window.location.href = 'display.php?city=' + city + '&country=' + country + '&profession=' + profession;
+	});
+</script>
 </body>
 </html>
